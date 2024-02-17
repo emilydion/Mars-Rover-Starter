dbee2858e7347cced8c291c7e92bef484bff56b5
emilydion <edion468@gmail.com>
2024-02-17 12:25:33 -0600

    message.spec.js tests are passing

--- message.js
+++ message.js
@@ -2,8 +2,13 @@ class Message {
    // has a name, contains several command objects
    // bundling commands from mission control and delivers them to rover
    constructor(name, commands) {
-
+      this.name = name;
+      if (!name) {
+         throw Error("Name required.");
+      }
+      this.commands= commands;
    }
+
 }
 
 module.exports = Message;
